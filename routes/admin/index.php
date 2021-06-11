002669268586f39b9335c5b3a82361a9d5020a2e
Nova Dwi Sapta Nain Seven <novadwisaptans@gmail.com>
2021-06-11 11:54:32 +0800

    Update

--- routes/admin/index.php
+++ routes/admin/index.php
@@ -3,6 +3,7 @@
 include_once __DIR__ . '/dashboard.php';
 include_once __DIR__ . '/master_data.php';
 include_once __DIR__ . '/pegawai.php';
+include_once __DIR__ . '/pembaruan_sk.php';
 include_once __DIR__ . '/duk.php';
 include_once __DIR__ . '/masa_kerja.php';
 include_once __DIR__ . '/penghargaan.php';
